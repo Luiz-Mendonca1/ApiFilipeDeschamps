7db3378179906f9e4f64905a43ec03d2a469affb
Luiz Mendonça <luizeduardomendonca77@gmail.com>
2025-10-08 10:06:14 -0300

    fix erro de versao mongoose

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const {Person} = require('./person')
+const dotenv = require('dotenv')
+const connectToDatabase = require('./src/database/connect')
 
+dotenv.config()
+
+connectToDatabase()
 // require('./modules/path')
 // require('./modules/fs')
 // require('./modules/http')
-require('./modules/express')
+// require('./modules/express')
 
-const luize = new Person('Luiz Eduardo');
+// const luize = new Person('Luiz Eduardo');
--- src/database/connect.js
+++ src/database/connect.js
@@ -1,13 +1,12 @@
-const mongose = require('mongoose');
+const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
-    await mongose.connect('mongodb+srv://admin:<db_password>@cursonodejsfeliperocha.gt0vvqg.mongodb.net/?retryWrites=true&w=majority&appName=CursoNodeJSFelipeRocha',
-        (error)=>{
-            if(error){
-                console.log('Erro ao conectar com o banco de dados:', error);
-            } else {
-                console.log('Conectado ao banco de dados com sucesso!');
-            }
-        });
-}
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejsfeliperocha.gt0vvqg.mongodb.net/database?retryWrites=true&w=majority&appName=CursoNodeJSFelipeRocha`);
+        console.log('Conectado ao banco de dados com sucesso!');
+    } catch (error) {
+        console.log('Erro ao conectar com o banco de dados:', error);
+    }
+};
+
 module.exports = connectToDatabase;
